347bf3bca309f3ad83a910606e1018a1929fa8cb
Jan Dziewulski <jan.dziewulski92@gmail.com>
2023-09-13 01:16:13 +0200

    poprawiony kod

--- definiowanie_manualne.py
+++ definiowanie_manualne.py
@@ -1,31 +1,37 @@
 import cv2
 
-# Lista do przechowywania wybranych punktów
-points = []
+# Zmienne globalne
+drawing = False  # flaga oznaczająca czy rysowanie jest aktywne
+ix, iy = -1, -1  # inicjalne koordynaty punktu początkowego
 
+# Funkcja obsługująca zdarzenia myszy
 def draw_rectangle(event, x, y, flags, param):
-    global points, image
-    if event == cv2.EVENT_LBUTTONDOWN:
-        points.append((x, y))
-        cv2.circle(image, (x, y), 5, (0, 255, 0), -1)
-        cv2.imshow('Obraz', image)
+    global ix, iy, drawing, image
 
-        # Jeżeli wybrano dwa punkty, rysuj prostokąt
-        if len(points) == 2:
-            cv2.rectangle(image, points[0], points[1], (0, 255, 0), 2)
-            cv2.imshow('Obraz', image)
-            points.clear()
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(image, (ix, iy), (x, y), (0, 255, 0), 2)
+        cv2.imshow('image', image)
 
 # Wczytanie obrazu
-image_path = 'sciezka_do_obrazu.jpg'
-image = cv2.imread(image_path)
+image = cv2.imread('sciezka_do_obrazu.jpg')
 
-# Wyświetlenie obrazu
-cv2.imshow('Obraz', image)
+# Tworzenie okna
+cv2.namedWindow('image')
 
-# Ustalenie funkcji callback dla zdarzeń myszy
-cv2.setMouseCallback('Obraz', draw_rectangle)
+# Przypisanie funkcji obsługującej zdarzenia myszy
+cv2.setMouseCallback('image', draw_rectangle)
 
-# Oczekiwanie na zamknięcie okna
-cv2.waitKey(0)
+while(1):
+    cv2.imshow('image', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 cv2.destroyAllWindows()
+
+# Zapisanie obrazu
+cv2.imwrite('image_with_rectangle.jpg', image)
